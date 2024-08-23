6535d064d638f59fe28be330ee52a60b3f2503ba
kartiik22 <kartikspics@gmail.com>
2024-08-23 06:53:51 +0530

    Final Commit

---
+++ app/src/main/java/com/example/endurofit/FiveDaySplit.java
@@ -0,0 +1,14 @@
+package com.example.endurofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiveDaySplit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_day_split);
+    }
+}
